d7dad12c725d5d8e7c1f13aa6766785178d6e1f5
vanmitG <mit.van@gmail.com>
2019-07-31 07:37:41 +0700

    initial commmit

---
+++ README.md
@@ -0,0 +1,58 @@
+# CoderSchool FTW - _ A Todo App _
+
+Created with love by: An-Khuong Huynh
+
+This is a simple todo app that requires a solid understanding of the principles of JavaScript programming to customize.
+
+<!-- ## Video Walkthrough -->
+
+Here's a walkthrough of implemented user stories.
+
+![](img/screenShot.png)
+
+<!-- ## Code Reviews -->
+
+<!-- This code was reviewed by @username and @otherusername.
+
+- [Link to PR #X](https://github.com/vanmitG/canvas_game_starter/pull/1) - reviewed by @username.
+- [Link to PR #Y](#) - reviewed by @otherusername. -->
+
+## Required User Stories
+
+<!-- - [ ] I have at least three code reviews from others. -->
+
+- [x] The user can add todo by enter text into input box and either click ADD or Enter key
+- [x] The Use can remove todo items
+- [x] The user can toggle between Done or Not Done on todo items
+- [x] The todo items is stored in local storage for future uses
+- [x] The user can filter todo items by Done, Not Done or All by click on appropriate button
+
+<!-- ## Optional User Stories
+
+
+
+The following **additional** features are implemented:
+
+- [x] List anything else that you can get done to improve the page!
+
+## Time Spent and Lessons Learned
+
+Time spent: **X** hours spent in total.
+
+Describe any challenges encountered while building the app. -->
+
+## License
+
+    Copyright [yyyy] [name of copyright owner]
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
---
+++ script.js
@@ -0,0 +1,103 @@
+console.log("script.js");
+document.getElementById("inputTodo").focus();
+let todoList = localStorage.getItem("todoList");
+if (!todoList) {
+  todoList = [
+    { body: "task 1", done: false },
+    { body: "task 2", done: true },
+    { body: "task 3", done: true }
+  ];
+}
+
+function createTodoObject() {
+  let body = document.getElementById("inputTodo").value;
+  console.log(`L 11- addTodo value: ${body}`);
+  const todoObject = {
+    body,
+    done: false
+  };
+  document.getElementById("inputTodo").value = "";
+  return todoObject;
+  //TODO: add clear input here
+}
+
+function addTodo() {
+  const newTodo = createTodoObject();
+  todoList.push(newTodo);
+  console.log(`todoList L-24: ${newTodo.body}`);
+  renderTodoList(todoList);
+  document.getElementById("inputTodo").focus();
+}
+
+function keyDowndInInput() {
+  if (event.keyCode == 13) {
+    document.getElementById("addClick").click();
+  }
+}
+
+// function renderSingleTodo(newTodo) {
+//   let node = document.createElement("LI");
+//   let delNode = document.createElement("SPAN");
+//   let textnode = document.createTextNode(newTodo.body);
+//   let delIcon = document.createTextNode("X");
+//   node.appendChild(textnode);
+//   delNode.appendChild(delIcon);
+//   node.appendChild(delNode);
+//   document.getElementById("todoList").appendChild(node);
+// }
+
+function renderTodoList(filteredTodoList) {
+  document.getElementById("todoList").innerHTML = "";
+  const todoHtml = filteredTodoList.map((todo, idx) => {
+    const lineThrough = "";
+    return `<li class="list-group-item ${
+      todo.done ? " list-group-item-secondary" : "list-group-item-success"
+    } d-flex justify-content-between align-items-center"><h5 class="${
+      todo.done ? "todo-done" : ""
+    }" onclick=onToggle(${idx})>${
+      todo.body
+    }</h5><span><a class="btn btn-outline-success" href="#" onclick=onToggle(${idx})><i class="fa ${
+      todo.done ? "fa-check-square" : "fa-square-o"
+    }" aria-hidden="true"></i></a><a class="btn btn-outline-danger" href="#" onclick=onDelete(${idx})><i class="fa fa-trash" aria-hidden="true"></i></a></span></li>`;
+  });
+  document.getElementById("todoList").innerHTML = todoHtml.join("");
+  localStorage.setItem("todoList", filteredTodoList);
+  // console.log(`line tod oHtml ${todoHtml}:`);
+}
+
+function onDelete(idx) {
+  // console.log(`User Click Delete - line58 -idx=${idx}`);
+  todoList.splice(idx, 1);
+  renderTodoList(todoList);
+}
+
+function onToggle(idx) {
+  const newStatus = !todoList[idx].done;
+  todoList[idx].done = newStatus;
+  console.log(
+    `User Click To Toggle - line63 - idx=${idx} and new status is ${newStatus}`
+  );
+  renderTodoList(todoList);
+}
+function doneTodo() {
+  const doneTodo = todoList.filter(todo => {
+    return todo.done === true;
+  });
+  console.log(`Line-81:User Click Done todo: ${doneTodo}`);
+  renderTodoList(doneTodo);
+}
+function notDoneTodo() {
+  const notDoneTodo = todoList.filter(todo => {
+    return todo.done === false;
+  });
+  // });
+  console.log(`Line-88:User Click Not Done todo: `);
+  renderTodoList(notDoneTodo);
+}
+function allTodo() {
+  console.log(`Line-88:User Click all  todo: `);
+  renderTodoList(todoList);
+}
+
+renderTodoList(todoList);
+localStorage.setItem("todoList", todoList);
